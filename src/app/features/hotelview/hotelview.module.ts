d02df9d2c487cd83f6d8f28d7a7558aecbda86b6
Francesco <francesco.leuzzi@intelllitronika.com>
2022-09-19 19:07:21 +0200

    static hotels price lists

--- src/app/features/hotelview/hotelview.module.ts
+++ src/app/features/hotelview/hotelview.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DescriptionComponent } from './description/description.component';
 import { GooglemapComponent } from './googlemap/googlemap.component';
+import { PrezziComponent } from './prezzi/prezzi.component';
 
 
 
 @NgModule({
   declarations: [
     DescriptionComponent,
-    GooglemapComponent
+    GooglemapComponent,
+    PrezziComponent
   ],
   imports: [
     CommonModule
