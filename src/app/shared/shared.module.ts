040cf4143eef27d7ee11ebfa3dee6b0d0315bfd1
Francesco <francesco.leuzzi@intelllitronika.com>
2022-09-19 17:43:14 +0200

    fix posizioni componenti

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HotelCardComponent } from './components/hotel-card/hotel-card.component';
 import { GenericCardComponent } from './components/generic-card/generic-card.component';
 import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './components/login/login.component';
-import { ReviewComponent } from './components/review/review.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { ReservationComponent } from './components/reservation/reservation.component';
+import { LoginComponent } from '../features/components/login/login.component';
+import { ReviewComponent } from '../features/components/review/review.component';
+import { ContactComponent } from '../features/components/contact/contact.component';
+import { ReservationComponent } from '../features/components/reservation/reservation.component';
 
 
 
