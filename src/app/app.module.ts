efc405bc778ddcc0d44519acc44002e2a7348bd8
Francesco <francesco.leuzzi@intelllitronika.com>
2022-09-19 13:09:19 +0200

    navbar dynamic routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { CartComponent } from './features/cart/cart.component';
+import { LoginComponent } from './features/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CartComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
